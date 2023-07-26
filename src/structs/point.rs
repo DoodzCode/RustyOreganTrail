7c6be090cf1adef83756c7896039e2c5953eac5c
Kaleb Marquez-Cooke <kalebmarquez13@gmail.com>
2023-07-25 21:05:41 -0700

    pushing changes for debugging

--- src/structs/point.rs
+++ src/structs/point.rs
@@ -15,6 +15,16 @@ mod test {
 
     #[test]
     fn test_Point() {
-
+        let zone_id: new_V4();
+        let biome_id: new_V4();
+        let weather_id: new_V4();
+        let t_point = Point {
+            id: new_V4(),
+            zone_id = &zone_id,
+            biome_id: &biome_id,
+            weather_id: &weather_id,
+            description: String::new(),
+        };
+        assert_eq!(t_point.zone_id, zone_id);
     }
 }
