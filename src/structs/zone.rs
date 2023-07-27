6c9190f0a66c869bfdcdc06e9804916b44540179
Ian Deans <ideans715@gmail.com>
2023-07-26 23:37:47 -0700

    stash a mixup

--- src/structs/zone.rs
+++ src/structs/zone.rs
@@ -1,10 +1,31 @@
 use uuid::Uuid;
 
 #[derive(Debug)]
-pub struct Zone {
-    id: Uuid,
-    name: String,
+// pub struct Zone {
+//     id: Uuid,
+//     name: String,
+// }
+
+// impl Zone {
+//     pub fn new(name: &str) -> Zone {
+//         Zone {
+//             id: Uuid::new_v4(),
+//             name: String::from(name),
+//         }
+//     }
+// }
+//! Got Biomes and Zones mixed up. Will fix later.
+enum Zone {
+    Plains,
+    Hills,
+    Desert,
+    Forest,
+    Mountains,
 }
+
+
+
+
 
 #[cfg(test)]
 mod test {
@@ -12,13 +33,14 @@ mod test {
 
     #[test]
     fn test_Zone() {
-        let t_zone: Zone = Zone {
-            id: Uuid::new_v4(),
-            name: "Test Zone".to_string(),
-        };
+        let z_plains: Zone = Zone::Plains;
+        let z_hills: Zone = Zone::Hills;
+        let z_desert: Zone = Zone::Desert;
+        let z_forest: Zone = Zone::Forest;
+        let z_mountains: Zone = Zone::Mountains;
 
-        assert_eq!(t_zone.name, "Test Zone".to_string());
+        // assert_eq!(t_zone.name, "Test Zone".to_string());
 
-        dbg!(t_zone);
+        dbg!();
     }
 }
