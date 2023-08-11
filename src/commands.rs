9f6276e894fd947d51a8e0fa8ef8fb91c83409af
Kaleb Marquez-Cooke <kalebmarquez13@gmail.com>
2023-08-11 15:11:17 -0700

    idk

--- src/commands.rs
+++ src/commands.rs
@@ -1,176 +1,405 @@
-use crate::structs::{caravan::Caravan, trailpoint::TrailPoint, traits::Name};
-use std::collections::HashMap;
+use crate::common::{GameData, GatherRates, People, Wagon};
+use crate::structs::{
+    terrain::Terrain,
+    trailpoint::{Coords, TrailPoint},
+};
 
-// {
-//     "camp": Comamnd::Camp,
-//     "key": "value",
-//     "key": "value",
-//     "key": "value",
-//     "key": "value",
-//     "key": "value",
+use colored::Colorize;
 
-// }
-pub enum Command {
-    Camp,
-    CheckAttire,
-    CheckCaravan,
-    CheckState,
-    ListCommands,
-    Look,
-    LookMap,
-    NotFound,
-    Quit,
-}
+pub fn match_command(cmd: String, game_data: &mut GameData) {
+    match cmd.as_str() {
+        "camp" => cmd_camp(game_data),
+        "gather" => cmd_gather_rate(&game_data.gather_rates),
+        "inspect" => cmd_inspect(&game_data.wagon),
+        "look" => cmd_look(&game_data.current_location.unwrap()),
+        "peep" => cmd_population_report(&game_data.people),
+        "survey" => println!(
+            "{:?}",
+            game_data
+                .current_location.unwrap()
+                .terrain
+                .base_resource_availability()
+        ),
+        "trust" => cmd_inspect_trust_level(&game_data), // ? Is this weird?
+        "travel" => game_data.current_location = Some(game_data.trail_iterator.unwrap().next().unwrap()),
+        "status" => cmd_status(&game_data),
+        "map" => print_map(&game_data.current_location.unwrap().coords, &game_data.map),
 
-static COMMANDS: [&str;8] = [
-    "camp",
-    "checkattire",
-    "checkcaravan",
-    "checkstate",
-    "listcommands",
-    "look",
-    "lookmap",
-    "quit",
-];
-
-// ? Maybe rename this function with a more declarative name
-// ! so... many... mutable... references... can't be a good idea...
-fn do_command(
-    cmd: Command,
-    location: &TrailPoint,
-    caravan: &mut Caravan,
-    game_state: &mut GameState,
-) {
-    match cmd {
-        Command::Look => look(location),
-        Command::LookMap => look_map(location),
-        Command::NotFound => (),
-        Command::Quit => std::process::exit(0),
-        Command::CheckCaravan => check_caravan(caravan),
-        Command::CheckAttire => check_attire(caravan),
-        Command::CheckState => check_state(game_state),
-        Command::Camp => camp(caravan, game_state),
-        Command::ListCommands => list_commands(),
-    }
-}
-
-fn list_commands() {
-    let mut list_string: String = String::new();
-    COMMANDS.into_iter().for_each(|cmd| {
-        list_string += " ";
-        list_string += cmd;
-    });
-    println!("{}", list_string);
-}
-
-// ! fix this at a later date (never)
-fn camp(caravan: &mut Caravan, game_state: &mut GameState) {
-    // overnight events/deductions/replenishments take place
-    // reduce food, water, wood by number of population
-    // restore days_to_travel back to 12
-    // incremet game day
-    // inspect population
-    // perform camp activities
-    // foraging
-    // hunting
-    // fishing
-    // repairs
-    // entertainment
-    caravan.supplies.reduce_all_by(caravan.population);
-    game_state.refresh_day();
-    println!("You and the rest of the people make camp for the night...");
-}
-
-fn check_state(game_state: &GameState) {
-    println!("{}", game_state.display_state());
-}
-
-fn check_attire(caravan: &Caravan) {
-    println!("{}", caravan.population_attire.display());
-}
-
-fn look(location: &TrailPoint) {
-    println!("\nYou scan your surroundings:");
-    println!("{}\n", location.get_description());
-}
-
-fn look_map(location: &TrailPoint) {
-    println!(
-        "According to the map, you are in {}",
-        location.region.get_name()
-    )
-}
-
-fn check_caravan(caravan: &Caravan) {
-    println!("{}", caravan.display());
-}
-
-/// Converts user input to a Command enum
-fn find_command(text: String) -> Command {
-    match text.as_str() {
-        "camp" => Command::Camp,
-        "checkattire" => Command::CheckAttire,
-        "checkcaravan" => Command::CheckCaravan,
-        "checkstate" => Command::CheckState,
-        "listcommands" => Command::ListCommands,
-        "look" => Command::Look,
-        "lookmap" => Command::LookMap,
-        "quit" => Command::Quit,
-        _ => Command::NotFound,
+        "quit" => std::process::exit(0),
+        _ => println!("Unknown Command"),
     }
 
-}
+    pub fn cmd_look(current_location: &TrailPoint) {
+        println!("{}", current_location.get_description());
+    }
 
-pub fn process_command(
-    input: String,
-    location: &TrailPoint,
-    caravan: &mut Caravan,
-    game_state: &mut GameState,
-) {
-    let cmd: Command = find_command(input);
-    do_command( cmd, location, caravan, game_state )
-    
-}
+    pub fn cmd_survey(current_location: &TrailPoint) {
+        println!(
+            "{:?}",
+            current_location.terrain.base_resource_availability()
+        );
+    }
 
-// ! move to its own module
-pub struct GameState {
-    pub days: u32,
-    pub miles_travelled: u32,
-    pub travel_hours_left_in_day: u8,
-}
+    pub fn cmd_inspect(wagon: &Wagon) {
+        println!("{:?}", wagon);
+    }
 
-impl GameState {
-    pub fn display_state(&self) -> String {
-        format!(
+    pub fn cmd_population_report(people: &People) {
+        println!("{:?}", people);
+    }
+
+    pub fn cmd_inspect_trust_level(game_data: &GameData) {
+        println!("{:?}", game_data.trust_level);
+    }
+
+    pub fn cmd_gather_rate(gather_rates: &GatherRates) {
+        println!("{:?}", gather_rates);
+    }
+
+    /// increases the wagon's food stock by the amount of the food gather rate
+    pub fn gather_food(wagon: &mut Wagon, gather_rates: &GatherRates) {
+        wagon.food_stock += gather_rates.food;
+    }
+    /// increases the wagon's water stock by the amount of the water gather rate
+    pub fn gather_water(wagon: &mut Wagon, gather_rates: &GatherRates) {
+        wagon.water_stock += gather_rates.water;
+    }
+    /// increases the wagon's wood stock by the amount of the wood gather rate
+    pub fn gather_wood(wagon: &mut Wagon, gather_rates: &GatherRates) {
+        wagon.wood_stock += gather_rates.wood;
+    }
+
+    pub fn cmd_camp(gd: &mut GameData) {
+        println!("You tell the caravan to start breaking down and make camp...");
+
+        let mut workers_left = gd.people.population;
+        let mut current_day = gd.days_travelled;
+        let mut g_food: u8 = 0;
+        let mut g_water: u8 = 0;
+        let mut g_wood: u8 = 0;
+        let mut repairers: u8 = 0;
+
+        let mut entertainment_tonight: bool = false;
+
+        loop {
+            println!("what tasks do you want to perform?");
+            println!("Options: entertain, tasks");
+            let player_input = get_input();
+            if player_input == "entertain" {
+                entertainment_tonight = true;
+                break;
+            } else if player_input == "tasks" {
+                loop {
+                    println!("What Tasks do you want to partake in?");
+                    println!("Options: repair, gather, done");
+                    let player_input = get_input();
+
+                    match player_input.as_str() {
+                        "repair" => assign_repairers(&mut workers_left, &mut repairers),
+                        "gather" => assign_gatherers(
+                            &mut workers_left,
+                            &mut g_food,
+                            &mut g_water,
+                            &mut g_wood,
+                        ),
+                        "done" => break,
+                        _ => (),
+                    }
+                }
+                break;
+            } else {
+                println!("invalid input");
+            }
+        }
+
+        // run start_new_day()
+
+        // end_of_day()
+        if (entertainment_tonight == true) {
+            entertainment(&mut gd.people, &gd.gather_rates);
+        } else {
+            perform_tasks(&g_food, &g_water, &g_wood, &repairers, gd);
+        }
+        println!("The sun sinks below the horizon and you turn in for the night.");
+
+        // start_new_day()
+        // Resource Consumption: Reduce Stocks
+        gd.wagon.food_stock -= gd.people.population;
+        gd.wagon.water_stock -= gd.people.population;
+        gd.wagon.wood_stock -= gd.people.population;
+        current_day += 1;
+    }
+
+    pub fn perform_tasks(
+        g_food: &u8,
+        g_water: &u8,
+        g_wood: &u8,
+        repairers: &u8,
+        gd: &mut GameData,
+    ) {
+        for _ in 0..*g_food {
+            gather_food(&mut gd.wagon, &gd.gather_rates);
+        }
+
+        for _ in 0..*g_water {
+            gather_water(&mut gd.wagon, &gd.gather_rates);
+        }
+
+        for _ in 0..*g_wood {
+            gather_wood(&mut gd.wagon, &gd.gather_rates);
+        }
+
+        for _ in 0..*repairers {
+            repair_wagon(&mut gd.wagon, &gd.gather_rates)
+        }
+    }
+
+    /// This function performs the actions of a single worker repairing the wagon based on the repair_rate
+    pub fn repair_wagon(wagon: &mut Wagon, gather_rates: &GatherRates) {
+        let MAX_DURABILITY: u8 = 100;
+        // let repair_tool = 10;
+        if wagon.durability < MAX_DURABILITY {
+            wagon.durability += gather_rates.repair;
+        }
+    }
+
+    // ? this is allowed?
+    pub fn assign_repairers(workers_left: &mut u8, repairers: &mut u8) {
+        println!("How many workers shall repair the wagon?");
+        loop {
+            let player_input = get_input().parse::<u8>().unwrap();
+            if player_input <= *workers_left {
+                *repairers += player_input;
+                *workers_left -= player_input;
+                break;
+            } else {
+                println!("not enough workers to assign to task");
+            }
+        }
+    }
+
+    pub fn entertainment(people: &mut People, gather_rates: &GatherRates) {
+        println!("Choose how you shall entertain: Feast, Stories, Music/Dance");
+        let player_input = get_input();
+        loop {
+            if player_input == "Feast" {
+                // add loop?
+                people.morale += gather_rates.morale + 15;
+                break;
+            } else if player_input == "Music" {
+                people.morale += gather_rates.morale + 10;
+                break;
+            } else if player_input == "Stories" {
+                // add loop?
+                people.morale += gather_rates.morale + 5;
+                break;
+            } else {
+                println!("Huh?");
+            }
+        }
+    }
+
+    pub fn assign_gatherers(
+        workers_left: &mut u8,
+        g_food: &mut u8,
+        g_water: &mut u8,
+        g_wood: &mut u8,
+    ) {
+        println!("Assigning people to gathering tasks:");
+        println!("How many workers shall be assigned to food?");
+        loop {
+            let player_input = get_input().parse::<u8>().unwrap();
+            if player_input <= *workers_left {
+                *g_food += player_input;
+                *workers_left -= player_input;
+                break;
+            } else {
+                println!("not enough workers to assign to task");
+            }
+        }
+
+        println!("How many many workers shall be assigned to water?");
+        loop {
+            let player_input = get_input().parse::<u8>().unwrap();
+            if player_input <= *workers_left {
+                *g_water += player_input;
+                *workers_left -= player_input;
+                break;
+            } else {
+                println!("not enough workers to be assigned to water?");
+            }
+        }
+
+        println!("How many many workers shall be assigned to wood?");
+        loop {
+            let player_input = get_input().parse::<u8>().unwrap();
+            if player_input <= *workers_left {
+                *g_wood += player_input;
+                *workers_left -= player_input;
+                break;
+            } else {
+                println!("not enough workers to be assigned to wood?");
+            }
+        }
+
+        println!(
+            "Food: {}, Water: {}, Wood: {}, Remaining: {}",
+            g_food, g_water, g_wood, workers_left
+        );
+    }
+
+    /// Reports data on population, progress, and supplies
+    pub fn cmd_status(gd: &GameData) {
+        println!(
+            r"
+         ___                                                  ___                               _   
+        (  _`\                                               |  _`\                            ( )_ 
+        | |_) ) _ __   _      __   _ __   __    ___   ___    | (_) )   __   _ _      _    _ __ | ,_)
+        | ,__/'( '__)/'_`\  /'_ `\( '__)/'__`\/',__)/',__)   | ,  /  /'__`\( '_`\  /'_`\ ( '__)| |  
+        | |    | |  ( (_) )( (_) || |  (  ___/\__, \\__, \   | |\ \ (  ___/| (_) )( (_) )| |   | |_ 
+        (_)    (_)  `\___/'`\__  |(_)  `\____)(____/(____/   (_) (_)`\____)| ,__/'`\___/'(_)   `\__)
+                           ( )_) |                                         | |                      
+                            \___/'                                         (_)                     
+        "
+        );
+
+        println!(
             "
-            +--------------------------------------+
-                Days Travelled: {days}
-            +--------------------------------------+
-                Miles Travelled: {miles}
-            +--------------------------------------+
-                Hours Before Nightfall {hours}
-            +--------------------------------------+
-            ",
-            days = self.days,
-            miles = self.miles_travelled,
-            hours = self.travel_hours_left_in_day
+    Trust Level: {trust}            Date: {date}
+    Population: {pop}               Days Travelled: {days}
+    ",
+            trust = gd.trust_level,
+            pop = gd.people.population,
+            date = "11/11/11".to_string(),
+            days = gd.days_travelled
         )
     }
 
-    pub fn ok_to_travel(&self) -> bool {
-        self.travel_hours_left_in_day > 1
+    fn print_map(coords: &Coords, map: &Vec<Vec<Terrain>>) {
+        let radius: u8 = 10;
+
+        let row_start: u8 = coords.0 - radius;
+        let col_start: u8 = coords.1 - radius;
+
+        let row_end: u8 = coords.0 + radius;
+        let col_end: u8 = coords.1 + radius;
+
+        let player_token = '*';
+
+        for y_coord in row_start..row_end + 1 {
+            for x_coord in col_start..col_end + 1 {
+                if x_coord == coords.1 && y_coord == coords.0 {
+                    print!("{} ", player_token.to_string().red());
+                } else {
+                    print_token(&map[usize::from(y_coord)][usize::from(x_coord)]);
+                }
+            }
+            print!("\n");
+        }
+
+        fn print_token(terrain: &Terrain) {
+            match terrain {
+                Terrain::Plains => print!(
+                    "{} ",
+                    terrain.get_token().to_string().bright_yellow().dimmed()
+                ),
+                Terrain::Desert => print!("{} ", terrain.get_token().to_string().red()),
+                Terrain::Forest => print!("{} ", terrain.get_token().to_string().green()),
+                Terrain::Hills => print!("{} ", terrain.get_token().to_string().yellow()),
+                Terrain::Mountain => print!("{} ", terrain.get_token().to_string()),
+                Terrain::Trail => print!("{} ", terrain.get_token().to_string().purple()),
+                _ => (),
+            }
+        }
+    }
+}
+
+pub fn _generate_map(rows: u8, cols: u8) -> Vec<Vec<Terrain>> {
+    let mut map: Vec<Vec<Terrain>> = Vec::new();
+
+    for y in 0..rows {
+        let mut row: Vec<Terrain> = Vec::new();
+        for x in 0..cols {
+            row.push(Terrain::Plains);
+        }
+        map.push(row);
     }
 
-    pub fn reduce_day_hours(&mut self, amount: u8) {
-        self.travel_hours_left_in_day -= amount;
-    }
+    map
+}
 
-    pub fn increase_miles(&mut self, amount: u32) {
-        self.miles_travelled += amount;
-    }
+/// Generates a square of forest around a single point
+pub fn build_forest(coords: (u8, u8), map: &mut Vec<Vec<Terrain>>, radius: u8) {
+    let row_start: u8 = coords.0 - radius;
+    let col_start: u8 = coords.1 - radius;
 
-    pub fn refresh_day(&mut self) {
-        self.days += 1;
-        self.travel_hours_left_in_day = 12;
+    let row_end: u8 = coords.0 + radius;
+    let col_end: u8 = coords.1 + radius;
+
+    let mut y: u8 = 0;
+    let mut x: u8 = 0;
+
+    for row in map.iter_mut() {
+        for row in map.iter_mut() {
+            for point in row.iter_mut() {
+                if y >= row_start && y <= row_end {
+                    if x >= col_start && x <= col_end {
+                        if y == row_start || y == row_end || x == col_start || x == col_end {
+                            if rand::random() {
+                                *point = Terrain::Forest;
+                            }
+                        } else {
+                            *point = Terrain::Forest;
+                        }
+                    }
+                }
+                x += 1;
+            }
+            y += 1;
+            x = 0;
+        }
     }
+}
+
+pub fn add_trail_to_map(map: &mut Vec<Vec<Terrain>>) {
+    let trail_col: u8 = 50;
+    let mut y: u8 = 0;
+    let mut x: u8 = 0;
+
+    for row in map.iter_mut() {
+        for point in row.iter_mut() {
+            if x == trail_col {
+                *point = Terrain::Trail;
+            }
+            x += 1;
+        }
+        y += 1;
+        x = 0;
+    }
+}
+
+pub fn get_input() -> String {
+    let mut r_input: String = String::new();
+    std::io::stdin().read_line(&mut r_input).unwrap();
+    let input: &str = r_input.trim();
+    String::from(input).to_lowercase()
+}
+
+pub fn opening_screen() {
+    println!(
+        r"
+$$$$$$$\                        $$\                      $$$$$$\                                                          $$$$$$$$\                 $$\ $$\ 
+$$  __$$\                       $$ |                    $$  __$$\                                                         \__$$  __|                \__|$$ |
+$$ |  $$ |$$\   $$\  $$$$$$$\ $$$$$$\   $$\   $$\       $$ /  $$ | $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$$$$$$\           $$ | $$$$$$\  $$$$$$\  $$\ $$ |
+$$$$$$$  |$$ |  $$ |$$  _____|\_$$  _|  $$ |  $$ |      $$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\          $$ |$$  __$$\ \____$$\ $$ |$$ |
+$$  __$$< $$ |  $$ |\$$$$$$\    $$ |    $$ |  $$ |      $$ |  $$ |$$ |  \__|$$$$$$$$ |$$ /  $$ |$$ /  $$ |$$ |  $$ |         $$ |$$ |  \__|$$$$$$$ |$$ |$$ |
+$$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\ $$ |  $$ |      $$ |  $$ |$$ |      $$   ____|$$ |  $$ |$$ |  $$ |$$ |  $$ |         $$ |$$ |     $$  __$$ |$$ |$$ |
+$$ |  $$ |\$$$$$$  |$$$$$$$  |  \$$$$  |\$$$$$$$ |       $$$$$$  |$$ |      \$$$$$$$\ \$$$$$$$ |\$$$$$$  |$$ |  $$ |         $$ |$$ |     \$$$$$$$ |$$ |$$ |
+\__|  \__| \______/ \_______/    \____/  \____$$ |       \______/ \__|       \_______| \____$$ | \______/ \__|  \__|         \__|\__|      \_______|\__|\__|
+                                        $$\   $$ |                                    $$\   $$ |                                                            
+                                        \$$$$$$  |                                    \$$$$$$  |                                                            
+                                         \______/                                      \______/
+"
+    );
+    println!("Press Enter to Continue");
 }
---
+++ src/common.rs
@@ -0,0 +1,132 @@
+use crate::structs::{
+    trailpoint::{TrailPoint, _generate_tiny_trail},
+    terrain::Terrain,
+};
+
+use crate::commands::{_generate_map, add_trail_to_map, build_forest};
+
+
+/// Holds all 'global' data for the game
+#[derive(Debug)]
+pub struct GameData {
+    /// Struct representing a wagon which holds resources and has durability
+    pub wagon: Wagon,
+    /// Struct that represents the people
+    pub people: People,
+    /// Struct used to hold any "rates" at which stats are modified: Needs a better name
+    pub gather_rates: GatherRates,
+
+    pub trail: Vec<TrailPoint>,
+    pub trail_iterator: Option<std::vec::IntoIter<TrailPoint>>, // TODO Make this field private
+    pub current_location: Option<TrailPoint>,
+    pub map: Vec<Vec<Terrain>>,
+    
+    // Needs refactoring
+    pub cold_resist: u8,
+    pub heat_resist: u8,
+    /// Represents the caravan population's trust in the player as a leader
+    pub trust_level: u8,
+
+    pub axes_in_inventory: u8,
+    pub knives_in_inventory: u8,
+    pub hammers_in_inventory: u8,
+
+    // location: (u8, u8), Deprecated: The current location is now tracked through the TrailPoints
+    /// How many miles the caravan has travelled thus far
+    pub miles_travelled: u32,
+    /// How many game days the player has been playing
+    pub days_travelled: u8,
+}
+
+impl GameData {
+    pub fn new_test_data() -> GameData {
+
+        let mut gd = GameData {
+            trail: _generate_tiny_trail(),
+            map: _generate_map(100, 100),
+            trail_iterator: None,
+            current_location: None,
+            people: People {
+                population: 20,
+                injured_population: 1,
+                sick_population: 2,
+                morale: 0,
+            },
+            gather_rates: GatherRates {
+                food: 5,
+                water: 5,
+                wood: 5,
+                repair: 5,
+                morale: 5,
+            },
+            trust_level: 50,
+            cold_resist: 20,
+            heat_resist: 40,
+            wagon: Wagon {
+                durability: 100,
+                food_stock: 15,
+                wood_stock: 15,
+                water_stock: 15,
+            },
+            axes_in_inventory: 5,
+            knives_in_inventory: 5,
+            hammers_in_inventory: 2,
+            // location: (50, 50),
+            miles_travelled: 0,
+            days_travelled: 0,
+        };
+
+        gd.mod_map();
+        gd.build_trail_iterator();
+        gd
+    }
+
+    fn mod_map(&mut self) {
+        build_forest((55, 45), &mut self.map, 4);
+        build_forest((40, 55), &mut self.map, 3);
+        build_forest((35, 50), &mut self.map, 2);
+        add_trail_to_map(&mut self.map);
+    }
+
+    fn build_trail_iterator(&mut self) {
+        self.trail_iterator = Some(self.trail.into_iter());
+        self.current_location = Some(self.trail_iterator.unwrap().next().unwrap())
+        // TODO unwrap circumvents error catching
+    }
+
+    pub fn get_next_location(&mut self) {
+        let t_iter: std::vec::IntoIter<&TrailPoint>;
+        match &mut self.trail_iterator {
+            Some(t_iter) => {
+                self.current_location = Some(t_iter.next().unwrap());
+            },  
+            None => ()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Wagon {
+    pub durability: u8,
+    pub food_stock: u8,
+    pub wood_stock: u8,
+    pub water_stock: u8,
+}
+
+#[derive(Debug)]
+pub struct People {
+    pub population: u8,
+    pub injured_population: u8,
+    pub sick_population: u8,
+    pub morale: i8,
+}
+
+// TODO rename this struct: we now have rates other than gathering
+#[derive(Debug)]
+pub struct GatherRates {
+    pub food: u8,
+    pub water: u8,
+    pub wood: u8,
+    pub repair: u8,
+    pub morale: i8,
+}
